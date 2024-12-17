2b7879dd52dac640bf6709fd10489b439f5e3074
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2024-12-17 09:53:42 +0100

    commit

--- linkedin-proejct/src/App.jsx
+++ linkedin-proejct/src/App.jsx
@@ -1,24 +1,24 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "./ProfileAside.css";
-import ProfileAside from "./components/ProfileAside";
-import MyNav from "./components/MyNav";
-import HomeMain from "./components/HomeMainComponents";
-import ProfilePage from "./components/ProfilePage";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import './ProfileAside.css'
+import ProfileAside from './components/ProfileAside'
+import MyNav from './components/MyNav'
+import HomeMain from './components/HomeMainComponents'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
-	return (
-		<>
-			<header>
-				<MyNav />
-			</header>
-			<main>
-				<ProfilePage />
-				<HomeMain />
-				<ProfileAside />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <MyNav />
+      </header>
+      <main>
+        <ProfilePage />
+        <HomeMain />
+        <ProfileAside />
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
